4721637ec8994f9384745ca9beee4d8c6ea66ff9
justsimple <stevealane.damennjateng.ictuniversity.edu.cm>
2025-07-16 08:56:09 +0200

    redtfyguhiojpkl

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-# Apply database migrations
 python manage.py migrate
 python manage.py createsuperuser --noinput || true
-# Start Gunicorn
+
 gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --workers 3 &
 
-# Start Celery worker
 celery -A backend worker --loglevel=INFO &
 
-# Keep container running
 wait
