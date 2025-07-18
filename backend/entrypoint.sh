dc25ecdd567df8b1770cc45d16abd69e959977a7
justsimple <stevealane.damennjateng.ictuniversity.edu.cm>
2025-07-18 09:23:38 +0200

    modified:   college-companion/src/components/ui/Button.jsx modified:   college-companion/src/pages/signupForm.jsx modified:   .dockerignore modified:   backend/backend/settings.py modified:   backend/entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 echo "🔄 Running migrations..."
 python /app/manage.py makemigrations --noinput
 python /app/manage.py migrate --noinput
+python manage.py migrate api
 
 echo "📦 Collecting static files..."
 python /app/manage.py collectstatic --noinput
