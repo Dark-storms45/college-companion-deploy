703d63db02a626f31d9dd7884278fa784efb669b
justsimple <stevealane.damennjateng.ictuniversity.edu.cm>
2025-07-22 06:51:10 +0200

    feature: Update settings and environment variables for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -65,7 +65,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    'https://college-companion-8lcp.onrender.com',
+    'https://college-companion-v02d.onrender.com',
 
 
 ]
@@ -90,7 +90,7 @@ CORS_ALLOW_HEADERS = [
 CSRF_TRUSTED_ORIGINS=[
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://college-companion-8lcp.onrender.com'
+    'https://college-companion-v02d.onrender.com'
 
 ]
 
