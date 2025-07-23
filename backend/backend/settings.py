7daa8c53e63410dd1b4e0a7bb18ac02ed562f3b6
justsimple <stevealane.damennjateng.ictuniversity.edu.cm>
2025-07-23 09:04:17 +0200

    som changes made to the project files. These changes include: - Updated the author meta tag in `index.html`. - Added a link to an icon image in `index.html`. - Updated the Vite configuration to include a PWA plugin. - Changed the favicon link in `index.html`. - Updated the backend settings to include a new trusted origin.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -65,7 +65,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    'https://college-companion-v02d.onrender.com',
+    'https://college-companion-beta-p1wb.onrender.com',
 
 
 ]
@@ -90,7 +90,7 @@ CORS_ALLOW_HEADERS = [
 CSRF_TRUSTED_ORIGINS=[
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://college-companion-v02d.onrender.com'
+    'https://college-companion-beta-p1wb.onrender.com'
 
 ]
 
